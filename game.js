dfc885b7729455c56bef09f0149915f947e74626
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-14 13:24:41 +0530

    animate buttton

--- game.js
+++ game.js
@@ -1,12 +1,30 @@
 var buttonColours = ['red', 'blue', 'green', 'yellow'];
-var randomChosenColour=buttonColours[nextSequence()];
+var randomChosenColour = buttonColours[nextSequence()];
+var gamePattern = [];
+gamePattern.push(randomChosenColour);
 
+//==============Animate Button(Done)====================
+
+$('#' + randomChosenColour).animate(
+  {
+    opacity: 0.1,
+  },
+  150,
+  function () {
+    $('#' + randomChosenColour).animate(
+      {
+        opacity: 1,
+      },
+      150
+    );
+  }
+);
 
 //===========random Number(Dome)===============
 
-function nextSequence(){
-    var randomNumber=Math.floor(Math.random()*4)
-    return randomNumber;
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
 }
 
-console.log(randomChosenColour);
+console.log(gamePattern);
