8d4524bdbd903d2c6bbafa9e61ffdb540e3c48ff
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-14 13:05:40 +0530

    create array

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
+var buttonColours = ['red', 'blue', 'green', 'yellow'];;
+
+
+//===========random Number(Dome)===============
+
 function nextSequence(){
     var randomNumber=Math.floor(Math.random()*4)
     return randomNumber;
 }
-
-console.log(nextSequence());
