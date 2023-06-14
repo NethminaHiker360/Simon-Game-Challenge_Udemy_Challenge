ec54978e5515d7768515d3f8f41647bf34b7ab55
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-14 22:08:46 +0530

    end 06.14

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var buttonColours = ['red', 'blue', 'green', 'yellow'];
 var randomChosenColour = buttonColours[nextSequence()];
 var gamePattern = [];
-var userClickedPattern=[];
+var userClickedPattern = [];
 gamePattern.push(randomChosenColour);
 
 //==============Animate Button(Done)====================
@@ -23,20 +23,27 @@ $('#' + randomChosenColour).animate(
 
 //================Play Sound=================
 
-new Audio("sounds/"+randomChosenColour+".mp3").play();
-
+playSound(randomChosenColour);
 
 //============================
 
-$(".btn").click(function (e) { 
+$('.btn').click(function (e) {
   handler(this);
 });
 
-function handler(element){
-  var userChosenColour=$(element).attr("id");
+function handler(element) {
+  var userChosenColour = $(element).attr('id');
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+  playSound(userChosenColour);
 }
+
+//============Animate Press===============
+
+function animatePress(currentColour) {
+  
+}
+
+
 
 
 //===========random Number(Done)===============
@@ -44,4 +51,28 @@ function handler(element){
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   return randomNumber;
+}
+
+//================Play Sound()==========
+
+function playSound(name) {
+  switch (name) {
+    case 'blue':
+      new Audio('sounds/blue.mp3').play();
+      break;
+    case 'green':
+      new Audio('sounds/green.mp3').play();
+      break;
+    case 'red':
+      new Audio('sounds/red.mp3').play();
+      break;
+    case 'wrong':
+      new Audio('sounds/wrong.mp3').play();
+      break;
+    case 'yellow':
+      new Audio('sounds/yellow.mp3').play();
+      break;
+    default:
+      break;
+  }
 }
